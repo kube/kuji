a95b372c07648a964ad6d126bad75fa66a989800
Kube Khrm <kubekhrm@gmail.com>
2014-07-24 23:36:17 +0200

    Added _dependsOn function : GREEN

--- kuji.js
+++ kuji.js
@@ -1,10 +1,19 @@
 ﻿
 var kuji = {
 
+    // Helper for adding dependencies to task
+    _dependsOn: function (dependencies, task) {
+        task.dependencies = [];
+        for (var key in dependencies)
+            task.dependencies.push(dependencies[key]);
+        return task;
+    },
+
     graph: function (tasks) {
         for (var task in tasks)
             tasks[task]();
     }
+
 
 };
 
