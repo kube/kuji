66fc0d1135327266754990f4a0123128c8f7b0f5
Kube Khrm <kubekhrm@gmail.com>
2014-07-25 02:20:11 +0200

    GREEN: Passing single-dependency task

--- kuji.js
+++ kuji.js
@@ -9,11 +9,33 @@ var kuji = {
         return task;
     },
 
-    graph: function (tasks) {
-        for (var task in tasks)
-            tasks[task]();
-    }
 
+    graph: function (tasks) {
+        // Create graph of tasks
+        var root = [];
+        for (var i in tasks) {
+
+            var task = tasks[i];
+            task.promises = [];
+
+            if (!task.dependencies)
+                root.push(task);
+            else
+                for (var j in task.dependencies)
+                    tasks[task.dependencies[j]].promises.push(task);
+        }
+
+        // Run graph from root
+        for (var i in root) {
+            var task = root[i];
+
+            var next = function () {
+                for (var p in task.promises)
+                    task.promises[p]();
+            };
+            task(next);
+        }
+    }
 
 };
 
