579f6ee40e11c3333fac7dd3db65da4fe657f2da
Christophe FEIJOO <cfeijoo@e3r1p13.42.fr>
2014-09-28 02:19:16 +0200

    Linted JS code

--- kuji.js
+++ kuji.js
@@ -4,7 +4,7 @@
                                                   #########',##".
     by KUBE  : www.kube.io                       ##'##'##".##',##.
     Created  : Jul 23, 2014 6:55PM                ## ## ## # ##",#.
-    Modified : Jul 29, 2014 2:16AM                 ## ## ## ## ##'
+    Modified : Sep 28, 2014 2:17AM                 ## ## ## ## ##'
                                                     ## ## ## :##
                                                      ## ## ##*/
 
@@ -22,16 +22,16 @@ var Task = function (graph, task) {
             if (!_dependencies[i].finished)
                 return false;
         return true;
-    }
+    };
 
     this.addDependency = function (dependency) {
         _dependencies.push(dependency);
         dependency.addPromise(self);
-    }
+    };
 
     this.addPromise = function (promise) {
         _promises.push(promise);
-    }
+    };
 
     // Create Next callback
     var next = function () {
@@ -53,12 +53,12 @@ var Task = function (graph, task) {
             // Run task and pass it the callback
             task(next);
         }
-    }
+    };
 
-}
+};
 
 var Graph = function (finalCallback) {
-    
+
     var _nodes = [],
         _root = [],
         _finalCallback;
@@ -67,24 +67,24 @@ var Graph = function (finalCallback) {
         var node = new Task(this, task);
 
         // If task has no dependency add it to the graph root
-        if (!task.dependencies || task.dependencies.length == 0)
+        if (!task.dependencies || task.dependencies.length === 0)
             _root.push(node);
         // Else add each of its dependencies to the node
         else
             for (var i in task.dependencies)
                 node.addDependency(_nodes[task.dependencies[i]]);
         _nodes[name] = node;
-    }
+    };
 
     this.start = function () {
         // Start all tasks at root
         for (var i in _root)
             _root[i].start();
-    }
+    };
 
     this.setFinalCallback = function (finalCallback) {
-        _finalCallback = finalCallback;        
-    }
+        _finalCallback = finalCallback;
+    };
 
     this.tryFinish = function () {
         // Ensure there is a final callback
@@ -95,8 +95,8 @@ var Graph = function (finalCallback) {
             if (!_nodes[i].finished)
                 return;
             _finalCallback();
-    }
-}
+    };
+};
 
 
 var kuji = {
@@ -114,7 +114,7 @@ var kuji = {
         var _graph = new Graph();
 
         // Add all tasks to graph
-        for (var i in tasks) 
+        for (var i in tasks)
             _graph.addTask(i, tasks[i]);
 
         if (finalCallback)
